20097b1ec67b9c02f6b29548be9f05d3a26f0d43
Alexis Mousset <alexis.mousset@rudder.io>
2024-11-01 21:55:23 +0100

    Clean leftver

--- apt-pkg-c/types.h
+++ apt-pkg-c/types.h
@@ -8,12 +8,6 @@ using namespace rust;
 template <typename T>
 using UniquePtr = std::unique_ptr<T>;
 
-
-#include <cstddef>
-#include <memory>
-#include <type_traits>
-#include <utility>
-
 // Forward declarations for progress.rs
 struct ItemDesc;
 struct PkgAcquire;
